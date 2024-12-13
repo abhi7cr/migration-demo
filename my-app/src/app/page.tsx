51f778472dcb7f560c5a90ab0f371890390b4e40
rjabhi <rjabhi@amazon.com>
2024-12-12 22:01:42 -0800

    add nextjs frontend

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+'use client';
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from '../amplify_outputs.json';
+Amplify.configure(config);
+
+export function App({ signOut, user }: WithAuthenticatorProps) {
+  return (
+      <>
+        <h1>Hello {user?.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </>
+  );
+}
+
+export default withAuthenticator(App);
