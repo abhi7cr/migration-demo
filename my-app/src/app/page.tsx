aa4238a808f5204939a3fbc46c8a3489e7a8c781
rjabhi <rjabhi@amazon.com>
2024-12-12 22:35:09 -0800

    fix lint

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 'use client';
 import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
@@ -7,12 +6,12 @@ import '@aws-amplify/ui-react/styles.css';
 import config from '../amplify_outputs.json';
 Amplify.configure(config);
 
-export function App({ signOut, user }: WithAuthenticatorProps) {
+function App({ signOut, user }: WithAuthenticatorProps) {
   return (
-      <>
+      <div>
         <h1>Hello {user?.username}</h1>
         <button onClick={signOut}>Sign out</button>
-      </>
+      </div>
   );
 }
 
